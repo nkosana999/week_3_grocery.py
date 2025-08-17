aeeefe7071765b1a4b86270ad8e0adcb0886dd41
nkosana999 <pnnkosi17@gmail.com>
2025-08-16 23:44:52 -0700

    Updated grocery.py with bug fix

--- grocery.py
+++ grocery.py
@@ -4,14 +4,14 @@ def main():
     # A loop to continuos collection of user items into the list
     while True:
         try:
-            item_val = input("Item: ").upper()
+            item_val = input().upper()
             item_list.append(item_val)
         except EOFError:
             break
 
     item_set = set(item_list)
     # A loop to count the different items in the list and print how many times each appears
-    for item in item_set:
+    for item in sorted(item_set):
         count = item_list.count(item)
         print(f"{count} {item}")
 
